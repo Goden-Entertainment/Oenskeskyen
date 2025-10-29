a6ff41749a238ff10285a85d5687cc51f83e5632
Runeyung <runeyun@gmail.com>
2025-10-29 18:17:42 +0100

    Added static, templates and images.

--- src/main/java/org/example/oenskeskyen/controller/WishlistController.java
+++ src/main/java/org/example/oenskeskyen/controller/WishlistController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ public class WishlistController {
         this.wishlistService = wishlistService;
 
     }
+
 
     @GetMapping("list")
     public String getAllWishlist(Model model) {
